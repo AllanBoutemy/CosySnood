11c20f4ee449642fb05c9364fc18fdfad4c2c35a
AllanBoutemy <allanboutemy@hotmail.fr>
2023-12-25 16:27:00 +0100

    update comment main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,6 @@ for (let i = 1; i <= layerCount; i++) {
 
 function drawColorSquare(canvas, context, color) {
     context.clearRect(0, 0, 515, 325);
-    // console.log(color.r,color.g,color.b);
     context.fillStyle = `rgb(${color.r}, ${color.g}, ${color.b},0.5)`;
     context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -24,19 +23,15 @@ function drawColorSquare(canvas, context, color) {
 function applyColorToLayer(image, color,imagebase) {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d', { alpha: true });
-    const canvas2 = document.createElement('canvas');
-    const context2 = canvas2.getContext('2d', { alpha: true });
-    // Assurez-vous que le canvas a la même taille que l'image
+
     canvas.width = imagebase.width;
     canvas.height = imagebase.height;
 
-    canvas2.width = 30;
-    canvas2.height = 30;
 
-    // Dessinez l'image sur le canvas
+    // Dessinerl'image sur le canvas
     context.drawImage(imagebase, 0, 0, imagebase.width, imagebase.height);
 
-    // Obtenez les données de l'image du canvas
+    // Obtenir les données de l'image du canvas
     const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     const data = imageData.data;
 
@@ -45,30 +40,21 @@ function applyColorToLayer(image, color,imagebase) {
         if (data[i + 3] !== 0) {
             const lum = 0.3 * data[i] + 0.59 * data[i + 1] + 0.11 * data[i + 2];
             const alpha = data[i + 3] / 255;
-            const colorStrength = 0.2; // Ajustez cette valeur selon vos besoins
+            const colorStrength = 0.2; // Ajustez cette valeur pour l'intensité
 
             // Ajuster la couleur en préservant la luminosité
-            data[i] = Math.round((1 - colorStrength) * data[i] + color.r * colorStrength);   // Rouge
+            data[i] = Math.round((1 - colorStrength) * data[i] + color.r * colorStrength);// Rouge
             data[i + 1] = Math.round((1 - colorStrength) * data[i + 1] + color.g * colorStrength); // Vert
             data[i + 2] = Math.round((1 - colorStrength) * data[i + 2] + color.b * colorStrength); // Bleu
-        
-/*             color.r.alpha=0.2;
-            color.g.alpha=0.2;
-            color.b.alpha=0.2;
-            data[i] =color.r;   // Rouge
-            data[i + 1] = color.g; // Vert
-            data[i + 2] = color.b; // Bleu */
+    
         }
     }
     
-    // Mettez à jour les données de l'image sur le canvas
+    // Appliquer l'image sur le canvas
     context.putImageData(imageData, 0, 0);
 
-    // Remplacez l'image source par le canvas modifié
-    image.src = canvas.toDataURL();  // Dessinez le carré de couleur sur le deuxième canvas
-   /*  drawColorSquare(canvas2, context2, color); */
-
-
+    // Convertir l'image canvas vers une image src
+    image.src = canvas.toDataURL();  
 
 }
 function changeColors(color1, color2, color3) {
